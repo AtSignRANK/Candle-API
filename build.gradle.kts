44f8ee24c9905ee48857141af9cdf1d5e08e74a0
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-09-21 10:24:21 +0900

    [ = ] Shading

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,26 +21,11 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 }
 
+val shade = configurations.create("shade")
+shade.extendsFrom(configurations.implementation.get())
+
 tasks {
-    javadoc {
-        options.encoding = "UTF-8"
-    }
-
-    compileKotlin {
-        kotlinOptions.jvmTarget = "16"
-    }
-
-    create<Jar>("sourceJar") {
-        archiveClassifier.set("source")
-        from(sourceSets["main"].allSource)
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>(rootProject.name) {
-            from(components["java"])
-            artifact(tasks["sourceJar"])
-        }
-    }
+  jar {
+    from (shade.map { if (it.isDirectory) it else zipTree(it) })
+  }
 }
